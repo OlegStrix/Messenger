b878d939e0e323514491d186cf6c8c05267e4829
Oleg Orlovsky <35844912+OlegStrix@users.noreply.github.com>
2020-12-16 21:27:51 +0300

    Add nice photo

--- DotChatWF/MainForm.Designer.cs
+++ DotChatWF/MainForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.listMessages = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -42,21 +43,26 @@
             // 
             // listMessages
             // 
+            this.listMessages.BackColor = System.Drawing.Color.White;
+            this.listMessages.ForeColor = System.Drawing.Color.Black;
             this.listMessages.FormattingEnabled = true;
-            this.listMessages.ItemHeight = 16;
-            this.listMessages.Location = new System.Drawing.Point(16, 2);
-            this.listMessages.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listMessages.Location = new System.Drawing.Point(9, 10);
+            this.listMessages.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listMessages.Name = "listMessages";
-            this.listMessages.Size = new System.Drawing.Size(776, 340);
+            this.listMessages.Size = new System.Drawing.Size(250, 290);
             this.listMessages.TabIndex = 0;
             this.listMessages.SelectedIndexChanged += new System.EventHandler(this.listMessages_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 377);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-1, 305);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.Size = new System.Drawing.Size(78, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Username";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -64,9 +70,13 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 411);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(11, 340);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.Size = new System.Drawing.Size(65, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "Message";
             // 
@@ -74,35 +84,37 @@
             // 
             this.fieldUsername.AutoCompleteCustomSource.AddRange(new string[] {
             "You are not logged in"});
-            this.fieldUsername.BackColor = System.Drawing.SystemColors.Control;
-            this.fieldUsername.Location = new System.Drawing.Point(108, 374);
-            this.fieldUsername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.fieldUsername.BackColor = System.Drawing.Color.White;
+            this.fieldUsername.Location = new System.Drawing.Point(81, 304);
+            this.fieldUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fieldUsername.Name = "fieldUsername";
             this.fieldUsername.ReadOnly = true;
-            this.fieldUsername.Size = new System.Drawing.Size(355, 22);
+            this.fieldUsername.Size = new System.Drawing.Size(267, 20);
             this.fieldUsername.TabIndex = 3;
             this.fieldUsername.Text = "Not authenticated";
             this.fieldUsername.TextChanged += new System.EventHandler(this.fieldUsername_TextChanged);
             // 
             // fieldMessage
             // 
-            this.fieldMessage.Location = new System.Drawing.Point(108, 409);
-            this.fieldMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.fieldMessage.BackColor = System.Drawing.Color.Snow;
+            this.fieldMessage.Location = new System.Drawing.Point(81, 332);
+            this.fieldMessage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fieldMessage.Multiline = true;
             this.fieldMessage.Name = "fieldMessage";
-            this.fieldMessage.Size = new System.Drawing.Size(520, 41);
+            this.fieldMessage.Size = new System.Drawing.Size(391, 34);
             this.fieldMessage.TabIndex = 4;
             this.fieldMessage.TextChanged += new System.EventHandler(this.fieldMessage_TextChanged);
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(644, 374);
-            this.btnSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSend.BackColor = System.Drawing.Color.White;
+            this.btnSend.Location = new System.Drawing.Point(483, 304);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(144, 110);
+            this.btnSend.Size = new System.Drawing.Size(108, 89);
             this.btnSend.TabIndex = 5;
             this.btnSend.Text = "SEND";
-            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.UseVisualStyleBackColor = false;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // updateLoop
@@ -113,31 +125,35 @@
             // 
             // btnAuth
             // 
-            this.btnAuth.Location = new System.Drawing.Point(12, 455);
-            this.btnAuth.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAuth.BackColor = System.Drawing.Color.Transparent;
+            this.btnAuth.Location = new System.Drawing.Point(9, 370);
+            this.btnAuth.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAuth.Name = "btnAuth";
-            this.btnAuth.Size = new System.Drawing.Size(141, 28);
+            this.btnAuth.Size = new System.Drawing.Size(106, 23);
             this.btnAuth.TabIndex = 6;
             this.btnAuth.Text = "Authentification";
-            this.btnAuth.UseVisualStyleBackColor = true;
+            this.btnAuth.UseVisualStyleBackColor = false;
             this.btnAuth.Click += new System.EventHandler(this.btnAuth_Click);
             // 
             // btnReg
             // 
-            this.btnReg.Location = new System.Drawing.Point(175, 455);
-            this.btnReg.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReg.BackColor = System.Drawing.Color.Transparent;
+            this.btnReg.Location = new System.Drawing.Point(131, 370);
+            this.btnReg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnReg.Name = "btnReg";
-            this.btnReg.Size = new System.Drawing.Size(141, 28);
+            this.btnReg.Size = new System.Drawing.Size(106, 23);
             this.btnReg.TabIndex = 7;
             this.btnReg.Text = "Registartion";
-            this.btnReg.UseVisualStyleBackColor = true;
+            this.btnReg.UseVisualStyleBackColor = false;
             this.btnReg.Click += new System.EventHandler(this.btnReg_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(811, 501);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(608, 407);
             this.Controls.Add(this.btnReg);
             this.Controls.Add(this.btnAuth);
             this.Controls.Add(this.btnSend);
@@ -147,7 +163,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listMessages);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
